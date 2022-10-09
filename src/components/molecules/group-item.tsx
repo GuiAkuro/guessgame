2425f104763db8c6f61ce6533c6dae56bf455d07
Guilherme Oliveira <g.alves.oliv@gmail.com>
2022-10-09 18:59:15 -0300

    feat: reorder

--- src/components/molecules/group-item.tsx
+++ src/components/molecules/group-item.tsx
@@ -1,3 +1,4 @@
+import { Reorder } from "framer-motion"
 import { ITeam } from "./match"
 
 interface IGroupItemProps {
@@ -6,12 +7,12 @@ interface IGroupItemProps {
 
 export const GroupItem = ({ team }: IGroupItemProps) => {
   return (
-    <div className="flex bg-zinc-800 p-4 rounded gap-4">
+    <Reorder.Item value={team} className="flex bg-zinc-800 p-4 rounded gap-4">
       <div className="bg-zinc-500 w-12 h-12 rounded"></div>
       <div>
         <div className="text-lg font-semibold text-gray-200">{team.name}</div>
         <div className="text-sm text-zinc-400">{team.region}</div>
       </div>
-    </div>
+    </Reorder.Item>
   ) 
 }
--- src/components/organisms/group.tsx
+++ src/components/organisms/group.tsx
@@ -1,3 +1,5 @@
+import { Reorder } from "framer-motion"
+import { useState } from "react"
 import { GroupItem } from "../molecules/group-item"
 import { ITeam } from "../molecules/match"
 
@@ -12,15 +14,20 @@ interface IGroupProps {
 }
 
 export const Group = ({ group }: IGroupProps) => {
+
+  const [teams, setTeams] = useState(group.teams)
+
+
   return (
     <div>
       <h3 className="text-xl font-semibold text-gray-200 mb-4 capitalize">{group.name}</h3>
 
-      <div className="flex flex-col gap-4">
-        { group.teams.map(team => (
+      <Reorder.Group axis="y" values={teams} onReorder={setTeams} className="flex flex-col gap-4">
+        { teams.map(team => (
           <GroupItem key={team.id} team={team} />
         ))}
-      </div>
+      </Reorder.Group>
+
     </div>
   ) 
 }
