51ab0c63731bf5520452b697af94798e6b109708
Guilherme Oliveira <g.alves.oliv@gmail.com>
2022-10-11 03:28:47 -0300

    feat: fix

---
+++ src/components/molecules/match-table-item.tsx
@@ -0,0 +1,30 @@
+import Image from "next/future/image"
+
+export const MatchTableItem = () => {
+  return (
+    <tr className="border-b border-zinc-800">
+      <td className="p-4" width="230">21/11 13:00</td>
+      <td className="p-4">
+        <div className="flex items-center justify-center gap-4">
+          <div className="flex items-center gap-4">
+            <div>Brasil</div>
+            <div className="relative h-12 w-12">
+              <Image src="/shields/CBF.svg" alt="CBF" fill className="absolute" />
+            </div>
+          </div>
+          <div className="text-lg">vs</div>
+          <div  className="flex items-center gap-4">
+            <div className="relative h-12 w-12">
+              <Image src="/shields/FEF.svg" alt="CBF" fill className="absolute" />
+            </div>
+            <div>Equador</div>
+          </div>
+        </div>
+
+      </td>
+      <td className="p-4" width="230">
+        <button className="h-8 px-6 border border-green-600 text-green-600 text-xs rounded uppercase font-semibold">Fazer meu palpite</button>
+      </td>
+    </tr>
+  )
+}
---
+++ src/components/organisms/post.tsx
@@ -0,0 +1,37 @@
+import Image from "next/future/image"
+
+export const Post = () => {
+  return (
+    <div className="bg-zinc-800 p-4 rounded text-white">
+      <div className="flex items-center gap-4">
+        <div className="relative overflow-hidden w-10 h-10 rounded bg-zinc-700">
+          <Image src="/profile.jpg" alt="" fill className="absolute" />
+        </div>
+        <div className="text-lg">Guilherme Alves</div>
+      </div>
+      <div className="p-4">
+        <div className="flex items-center justify-center gap-4">
+          <div className="flex items-center gap-4">
+            <div className="flex bg-zinc-700 p-2 rounded gap-2 items-center">
+              <div className="p-2 bg-zinc-800 text-2xl rounded">3</div>
+              <div>Brasil</div>
+            </div>
+            <div className="relative h-12 w-12">
+              <Image src="/shields/CBF.svg" alt="CBF" fill className="absolute" />
+            </div>
+          </div>
+          <div className="text-lg">vs</div>
+          <div  className="flex items-center gap-4">
+            <div className="relative h-12 w-12">
+              <Image src="/shields/FEF.svg" alt="CBF" fill className="absolute" />
+            </div>
+            <div className="flex bg-zinc-700 p-2 rounded gap-2 items-center">
+              <div>Equador</div>
+              <div className="p-2 bg-zinc-800 text-2xl rounded">1</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
